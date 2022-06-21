f6ef0ba84e67a2ae85cb35ad7ae25905257cf721
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-21 19:09:03 +0530

    [Zeeshan] : added query to create table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -2,3 +2,15 @@
 
 CREATE DATABASE ADDRESSBOOKSERVICE;
 USE ADDRESSBOOKSERVICE;
+
+--UC2 Create AddresBook table
+CREATE TABLE AddressBook
+(
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    city VARCHAR(20),
+    state VARCHAR(20),
+    zip INT,
+    phone_number VARCHAR(10) NOT NULL
+);
