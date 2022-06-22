acaed62de0934dddb8b85b86bbc0d383a0abd4e6
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-22 06:21:22 +0530

    [Zeeshan] : added query to add values on given conditions

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -53,3 +53,25 @@ GROUP BY city;
 SELECT * FROM AddressBook 
 WHERE city = 'New York'
 ORDER BY first_name;
+
+--UC9 Add Type and Name information
+
+ALTER TABLE AddressBook
+ADD Book_Name VARCHAR(20),
+    Contact_Type VARCHAR(20);
+
+UPDATE AddressBook
+SET Contact_Type = 'Friends'
+WHERE first_name in ('Sue','Mike');
+
+UPDATE AddressBook
+SET Contact_Type = 'Professional'
+WHERE first_name in ('John','Liz');
+
+UPDATE AddressBook
+SET Contact_Type = 'Family'
+WHERE first_name in ('Meg');
+
+UPDATE AddressBook
+SET Book_Name = 'MyBook';
+
